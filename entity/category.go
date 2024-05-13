528e0d3eed2f64961a9194d4b3f0a967107ae5c1
Neniel <11655196+Neniel@users.noreply.github.com>
2024-05-13 19:12:11 -0300

    set values of date on database not in entity constructor

--- entity/category.go
+++ entity/category.go
@@ -11,13 +11,12 @@ type Category struct {
 	ID        primitive.ObjectID `bson:"_id" json:"id"`
 	Name      string             `bson:"name" json:"name"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt *time.Time         `bson:"updated_at" json:"updated_at"`
 }
 
 func NewCategory(name string) *Category {
 	return &Category{
-		ID:        primitive.NewObjectID(),
-		Name:      name,
-		CreatedAt: time.Now(),
+		Name: name,
 	}
 }
 
