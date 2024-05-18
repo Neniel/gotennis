8f2266a74bcd687cb2d99350910f9b7e6ce3f960
Neniel <11655196+Neniel@users.noreply.github.com>
2024-05-18 20:38:16 -0300

    update logger

--- logger/logger.go
+++ logger/logger.go
@@ -3,21 +3,42 @@ package logger
 import (
 	"log/slog"
 	"os"
-	"sync"
 )
 
-var once sync.Once
+var levelInfo map[string]slog.Level = map[string]slog.Level{
+	"DEBUG": slog.LevelDebug,
+	"":      slog.LevelInfo,
+	"INFO":  slog.LevelInfo,
+	"WARN":  slog.LevelWarn,
+	"ERROR": slog.LevelError,
+}
+
 var log *slog.Logger
 
 func init() {
-	once.Do(func() {
-		log = slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
-			AddSource: true,
-			Level:     slog.LevelInfo,
-		}))
-	})
+	log = slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+		AddSource: true,
+		Level:     levelInfo[os.Getenv("LOGGING_LEVEL")],
+	}))
 }
 
-func Logger() *slog.Logger {
-	return log
+func Debug(msg string, args ...any) {
+	log.Debug(msg, args...)
+}
+
+func Info(msg string, args ...any) {
+	log.Info(msg, args...)
+}
+
+func Warn(msg string, args ...any) {
+	log.Warn(msg, args...)
+}
+
+func Error(msg string, args ...any) {
+	log.Error(msg, args...)
+}
+
+func Fatal(msg string, args ...any) {
+	Error(msg, args...)
+	os.Exit(1)
 }
