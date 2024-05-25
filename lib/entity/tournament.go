337926fa6644440ac2715364bd947c005881d18c
Neniel <11655196+Neniel@users.noreply.github.com>
2024-05-24 21:03:11 -0300

    add tournament entity

---
+++ lib/entity/tournament.go
@@ -0,0 +1,16 @@
+package entity
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Tournament struct {
+	ID        primitive.ObjectID `bson:"_id" json:"id"`
+	Name      string             `bson:"name" json:"name"`
+	Location  string             `bson:"location" json:"location"`
+	StartDate time.Time          `bson:"start_date" json:"start_date"`
+	EndDate   time.Time          `bson:"end_date" json:"end_date"`
+	Category  *Category          `bson:"category" json:"category"`
+}
