64199b0b89d82dfc836e91ea8359fce5b6883389
Neniel <11655196+Neniel@users.noreply.github.com>
2024-05-28 20:55:36 -0300

    refactor players

--- players/api.go
+++ players/api.go
@@ -12,6 +12,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 
 	"github.com/Neniel/gotennis/lib/app"
+	"github.com/Neniel/gotennis/lib/database"
 	"github.com/Neniel/gotennis/lib/log"
 	"github.com/Neniel/gotennis/lib/telemetry/grafana"
 )
@@ -27,8 +28,8 @@ type Usecases struct {
 }
 
 type PlayerMicroservice struct {
-	App      app.IApp
-	Usecases *Usecases
+	App app.IApp
+	//Usecases *Usecases
 }
 
 type APIServer struct {
@@ -38,8 +39,8 @@ type APIServer struct {
 func (ms *PlayerMicroservice) NewAPIServer() *APIServer {
 	return &APIServer{
 		PlayerMicroservice: &PlayerMicroservice{
-			App:      ms.App,
-			Usecases: ms.Usecases,
+			App: ms.App,
+			//Usecases: ms.Usecases,
 		},
 	}
 
@@ -73,7 +74,24 @@ func (api *APIServer) pingHandler(w http.ResponseWriter, r *http.Request) {
 func (api *APIServer) listPlayers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Content-Type", "application/json")
-	categories, err := api.PlayerMicroservice.Usecases.ListPlayers.Do(r.Context())
+
+	/*
+	   1. recibir el token
+	   2. validar el token
+	   3. obtener datos del token
+	*/
+
+	customerID := "" // viene del token
+
+	client, ok := api.PlayerMicroservice.App.GetMongoDBClients()[customerID]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	listPlayers := usecase.NewListPlayers(database.NewDatabaseReader(client.MongoDBClient, client.DatabaseName))
+
+	categories, err := listPlayers.Do(r.Context())
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -90,7 +108,24 @@ func (api *APIServer) getPlayer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Content-Type", "application/json")
 	if categoryId := r.PathValue("id"); categoryId != "" {
-		categories, err := api.PlayerMicroservice.Usecases.GetPlayer.Do(r.Context(), categoryId)
+
+		/*
+		   1. recibir el token
+		   2. validar el token
+		   3. obtener datos del token
+		*/
+
+		customerID := "" // viene del token
+
+		client, ok := api.PlayerMicroservice.App.GetMongoDBClients()[customerID]
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		getPlayer := usecase.NewGetPlayer(database.NewDatabaseReader(client.MongoDBClient, client.DatabaseName))
+
+		categories, err := getPlayer.Do(r.Context(), categoryId)
 		if errors.Is(err, primitive.ErrInvalidHex) {
 			w.WriteHeader(http.StatusBadRequest)
 			grafana.SendMetric("get.player", 1, 1, map[string]interface{}{
@@ -134,7 +169,23 @@ func (api *APIServer) addPlayer(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	player, err := api.PlayerMicroservice.Usecases.CreatePlayer.Do(r.Context(), &request)
+	/*
+	   1. recibir el token
+	   2. validar el token
+	   3. obtener datos del token
+	*/
+
+	customerID := "" // viene del token
+
+	client, ok := api.PlayerMicroservice.App.GetMongoDBClients()[customerID]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	createPlayer := usecase.NewCreatePlayer(database.NewDatabaseWriter(client.MongoDBClient, client.DatabaseName), database.NewDatabaseReader(client.MongoDBClient, client.DatabaseName))
+
+	player, err := createPlayer.Do(r.Context(), &request)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte(err.Error()))
@@ -163,7 +214,23 @@ func (api *APIServer) updatePlayer(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		category, err := api.PlayerMicroservice.Usecases.UpdatePlayer.Do(r.Context(), id, &request)
+		/*
+		   1. recibir el token
+		   2. validar el token
+		   3. obtener datos del token
+		*/
+
+		customerID := "" // viene del token
+
+		client, ok := api.PlayerMicroservice.App.GetMongoDBClients()[customerID]
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		updatePlayer := usecase.NewUpdatePlayer(database.NewDatabaseWriter(client.MongoDBClient, client.DatabaseName), database.NewDatabaseReader(client.MongoDBClient, client.DatabaseName))
+
+		category, err := updatePlayer.Do(r.Context(), id, &request)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte(err.Error()))
@@ -192,7 +259,23 @@ func (api *APIServer) partiallyUpdatePlayer(w http.ResponseWriter, r *http.Reque
 			return
 		}
 
-		category, err := api.PlayerMicroservice.Usecases.PartiallyUpdatePlayer.Do(r.Context(), id, &request)
+		/*
+		   1. recibir el token
+		   2. validar el token
+		   3. obtener datos del token
+		*/
+
+		customerID := "" // viene del token
+
+		client, ok := api.PlayerMicroservice.App.GetMongoDBClients()[customerID]
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		partiallyUpdatePlayer := usecase.NewPartiallyUpdatePlayer(database.NewDatabaseWriter(client.MongoDBClient, client.DatabaseName), database.NewDatabaseReader(client.MongoDBClient, client.DatabaseName))
+
+		player, err := partiallyUpdatePlayer.Do(r.Context(), id, &request)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte(err.Error()))
@@ -200,7 +283,7 @@ func (api *APIServer) partiallyUpdatePlayer(w http.ResponseWriter, r *http.Reque
 		}
 
 		w.WriteHeader(http.StatusOK)
-		err = json.NewEncoder(w).Encode(&category)
+		err = json.NewEncoder(w).Encode(&player)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(err.Error()))
@@ -213,7 +296,23 @@ func (api *APIServer) deletePlayer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Header().Add("Content-Type", "application/json")
 	if id := r.PathValue("id"); id != "" {
-		err := api.PlayerMicroservice.Usecases.DeletePlayer.Do(r.Context(), id)
+
+		/*
+		   1. recibir el token
+		   2. validar el token
+		   3. obtener datos del token
+		*/
+
+		customerID := "" // viene del token
+
+		client, ok := api.PlayerMicroservice.App.GetMongoDBClients()[customerID]
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		deletePlayer := usecase.NewDeletePlayer(database.NewDatabaseWriter(client.MongoDBClient, client.DatabaseName))
+		err := deletePlayer.Do(r.Context(), id)
 		if errors.Is(err, primitive.ErrInvalidHex) {
 			w.WriteHeader(http.StatusBadRequest)
 			return
--- players/main.go
+++ players/main.go
@@ -3,26 +3,23 @@ package main
 import (
 	"context"
 
-	"github.com/Neniel/gotennis/players/usecase"
-
 	"github.com/Neniel/gotennis/lib/app"
-	"github.com/Neniel/gotennis/lib/database"
 )
 
 func main() {
 	app := app.NewApp(context.Background())
-	dbReader := database.NewDatabaseReader(app.GetMongoDBClient())
-	dbWriter := database.NewDatabaseWriter(app.GetMongoDBClient())
 
 	ms := &PlayerMicroservice{
 		App: app,
-		Usecases: &Usecases{
-			CreatePlayer: usecase.NewCreatePlayer(dbWriter, dbReader),
-			DeletePlayer: usecase.NewDeletePlayer(dbWriter),
-			ListPlayers:  usecase.NewListPlayers(dbReader),
-			GetPlayer:    usecase.NewGetPlayer(dbReader),
-			UpdatePlayer: usecase.NewUpdatePlayer(dbWriter, dbReader),
-		},
+		/*
+			Usecases: &Usecases{
+				CreatePlayer: usecase.NewCreatePlayer(dbWriter, dbReader),
+				DeletePlayer: usecase.NewDeletePlayer(dbWriter),
+				ListPlayers:  usecase.NewListPlayers(dbReader),
+				GetPlayer:    usecase.NewGetPlayer(dbReader),
+				UpdatePlayer: usecase.NewUpdatePlayer(dbWriter, dbReader),
+			},
+		*/
 	}
 
 	ms.NewAPIServer().Run()
