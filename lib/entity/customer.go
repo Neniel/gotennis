ae12df69464e83eea18ceadaf5747445d2d165f7
Neniel <11655196+Neniel@users.noreply.github.com>
2024-05-28 18:15:09 -0300

    add support for multiple db clients

---
+++ lib/entity/customer.go
@@ -0,0 +1,20 @@
+package entity
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Customer struct {
+	ID                      primitive.ObjectID `bson:"_id" json:"id"`
+	Name                    string             `bson:"name" json:"name"`
+	PhoneNumber             string             `bson:"phone_number" json:"phone_number"`
+	Email                   string             `bson:"email" json:"email"`
+	Tier                    string             `bson:"tier" json:"tier"`
+	MongoDBConnectionString string             `bson:"mongo_db_connection_string" json:"mongo_db_connection_string"`
+	CreatedBy               string             `bson:"created_by" json:"created_by"`
+	CreatedAt               time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt               *time.Time         `bson:"updated_at" json:"updated_at"`
+	UpdatedBy               *string            `bson:"updated_by" json:"updated_by"`
+}
