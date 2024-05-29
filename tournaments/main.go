24772bb27f82eaaaa39e123bfa60f91622a5ece8
Neniel <11655196+Neniel@users.noreply.github.com>
2024-05-28 21:00:25 -0300

    refactor tournament

--- tournaments/main.go
+++ tournaments/main.go
@@ -3,26 +3,23 @@ package main
 import (
 	"context"
 
-	"github.com/Neniel/gotennis/tournaments/usecase"
-
 	"github.com/Neniel/gotennis/lib/app"
-	"github.com/Neniel/gotennis/lib/database"
 )
 
 func main() {
 	app := app.NewApp(context.Background())
-	dbReader := database.NewDatabaseReader(app.GetMongoDBClient())
-	dbWriter := database.NewDatabaseWriter(app.GetMongoDBClient())
 
 	ms := &TournamentMicroservice{
 		App: app,
-		Usecases: &Usecases{
-			CreateTournament: usecase.NewCreateTournament(dbWriter),
-			DeleteTournament: usecase.NewDeleteTournament(dbWriter),
-			ListTournaments:  usecase.NewListTournaments(dbReader),
-			GetTournament:    usecase.NewGetTournament(dbReader),
-			UpdateTournament: usecase.NewUpdateTournament(dbWriter, dbReader),
-		},
+		/*
+			Usecases: &Usecases{
+				CreateTournament: usecase.NewCreateTournament(dbWriter),
+				DeleteTournament: usecase.NewDeleteTournament(dbWriter),
+				ListTournaments:  usecase.NewListTournaments(dbReader),
+				GetTournament:    usecase.NewGetTournament(dbReader),
+				UpdateTournament: usecase.NewUpdateTournament(dbWriter, dbReader),
+			},
+		*/
 	}
 
 	ms.NewAPIServer().Run()
