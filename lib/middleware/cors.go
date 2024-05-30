222f6e5c44f87363910dd09a4eabe9c2a6268f7f
Neniel <11655196+Neniel@users.noreply.github.com>
2024-05-29 22:56:15 -0300

    add middleware library

---
+++ lib/middleware/cors.go
@@ -0,0 +1,21 @@
+package middleware
+
+import "net/http"
+
+func CORSMiddleware(next http.Handler) http.Handler {
+	f := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Methods", "*")
+		w.Header().Add("Access-Control-Allow-Headers", "*")
+		w.Header().Add("Content-Type", "application/json")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(f)
+}
